563326b98bdfb14ba6d250d185ee641b916b7076
Guywithnoname <skyesimyikheng225183student@gmail.com>
2026-08-12 17:38:24 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata, Viewport } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Web Proxy - Access Freely',
-  description: 'Accessible web proxy for bypassing simple web restrictions',
+  title: 'test web proxy',
+  description: 'is cool ig',
   manifest: '/manifest.json',
   icons: {
     apple: '/apple-icon.png',
